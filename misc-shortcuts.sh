5f15add8a20cf7dd02ad39b9020b8dd04c50b0cf
Jake Archibald <jakearchibald@theteam.co.uk>
2011-09-12 15:47:54 +0100

    Importing shortcuts from current machine

---
+++ misc-shortcuts.sh
@@ -0,0 +1,6 @@
+alias ll='ls -l'
+alias share='python -c "import SimpleHTTPServer;SimpleHTTPServer.test()"'
+
+# Django shortcuts
+alias pym='python manage.py'
+alias pyms='pym runserver 0.0.0.0:8000'
