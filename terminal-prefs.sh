4d3fa2f6444aae7991506e8a0ac26aa6ade7fb0f
Jake Archibald <jaffathecake@gmail.com>
2011-09-15 08:55:29 +0100

    Adding git config

--- terminal-prefs.sh
+++ terminal-prefs.sh
@@ -1,3 +1,13 @@
+# Check for an interactive session
+[ -z "$PS1" ] && return
+
 # Pretty colours
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# Show tab-completion options on first <tab> instead of waiting
+# for multiple completions.
+set show-all-if-ambiguous on
+
+# Case insensitive tab-completion
+set completion-ignore-case on
